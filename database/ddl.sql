7e94fba46c379d87289f1d0fe4a822fefa767c40
fernandes-draw <fernandes_draw@hotmail.com>
2023-12-23 16:35:26 -0300

    add ddl script

---
+++ database/ddl.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS matchdiary;
+CREATE DATABASE matchdiary;
+
+USE matchdiary;
+
+-- Criando a tabela de times
+create TABLE team(
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+     state VARCHAR(2) NOT NULL CHECK (state IN ('AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
+    'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO')),
+    photo_url VARCHAR(1024) NOT NULL,
+    UNIQUE(name, state)
+);
+
+-- Criando a tabela de partidas
+CREATE TABLE match_tb(
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    team_one_id INTEGER NOT NULL REFERENCES team(id) ON UPDATE CASCADE ON DELETE NO ACTION,
+    score_team_one INTEGER NOT NULL, 
+    team_two_id INTEGER NOT NULL REFERENCES team(id) ON UPDATE CASCADE ON DELETE NO ACTION,
+    score_team_two INTEGER NOT NULL, 
+    team_supported_id INTEGER NOT NULL REFERENCES team(id) ON UPDATE CASCADE ON DELETE NO ACTION,
+    UNIQUE(date, team_one_id, team_two_id)
+);
+
+SELECT * FROM team;
+
+SELECT * FROM match_tb;
